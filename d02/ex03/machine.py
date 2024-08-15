936afa57abd5d6d0d8fb948a4e9df138a3f4c0e2
alex-sdn <alexsdnwcz@gmail.com>
2024-08-15 12:12:42 +0200

    ex03 ok

---
+++ d02/ex03/machine.py
@@ -0,0 +1,109 @@
+from beverages import HotBeverage, Coffee, Tea, Chocolate, Cappuccino
+import random
+
+class CoffeeMachine:
+    def __init__(self):
+        self.served = 0
+
+    class EmptyCup(HotBeverage):
+        name = 'empty cup'
+        price = 0.90
+
+        def description(self):
+            return 'An empty cup?! Gimme my money back!'
+        
+    class BrokenMachineException(Exception):
+        def __init__(self):
+            super().__init__('This coffee machine has to be repaired')
+
+    def repair(self):
+        self.served = 0
+
+    def serve(self, hotbeverage: HotBeverage):
+        if self.served < 10:
+            self.served += 1
+
+            if random.random() < 0.5:  # generates number between 0 and 1
+                return hotbeverage()
+            else:
+                return self.EmptyCup()  # count as served ?
+        else:
+            raise self.BrokenMachineException()
+
+if __name__ == '__main__':
+    machine = CoffeeMachine()
+
+    try:
+        beverage = machine.serve(HotBeverage)
+        print(beverage.__str__())
+        
+        beverage = machine.serve(HotBeverage)
+        print(beverage.__str__())
+
+        beverage = machine.serve(Coffee)
+        print(beverage.__str__())
+
+        beverage = machine.serve(Coffee)
+        print(beverage.__str__())
+        
+        beverage = machine.serve(Tea)
+        print(beverage.__str__())
+
+        beverage = machine.serve(Tea)
+        print(beverage.__str__())
+
+        beverage = machine.serve(Chocolate)
+        print(beverage.__str__())
+
+        beverage = machine.serve(Chocolate)
+        print(beverage.__str__())
+
+        beverage = machine.serve(Cappuccino)
+        print(beverage.__str__())
+
+        beverage = machine.serve(Cappuccino)
+        print(beverage.__str__())
+
+        beverage = machine.serve(HotBeverage)
+        print(beverage.__str__())
+    except Exception as e:
+        print(e)
+
+    print('\n>REPAIRING<\n')
+    machine.repair()
+
+    try:
+        beverage = machine.serve(HotBeverage)
+        print(beverage.__str__())
+        
+        beverage = machine.serve(HotBeverage)
+        print(beverage.__str__())
+
+        beverage = machine.serve(Coffee)
+        print(beverage.__str__())
+
+        beverage = machine.serve(Coffee)
+        print(beverage.__str__())
+        
+        beverage = machine.serve(Tea)
+        print(beverage.__str__())
+
+        beverage = machine.serve(Tea)
+        print(beverage.__str__())
+
+        beverage = machine.serve(Chocolate)
+        print(beverage.__str__())
+
+        beverage = machine.serve(Chocolate)
+        print(beverage.__str__())
+
+        beverage = machine.serve(Cappuccino)
+        print(beverage.__str__())
+
+        beverage = machine.serve(Cappuccino)
+        print(beverage.__str__())
+
+        beverage = machine.serve(HotBeverage)
+        print(beverage.__str__())
+    except Exception as e:
+        print(e)
