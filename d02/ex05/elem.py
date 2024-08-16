592b7a22a01886de01afff3537f81568f4e3d63e
alex-sdn <alexsdnwcz@gmail.com>
2024-08-16 18:38:54 +0200

    move to sep file

--- d02/ex05/elem.py
+++ d02/ex05/elem.py
@@ -89,98 +89,15 @@ class Elem:
                 (type(content) == list and all([type(elem) == Text or
                                                 isinstance(elem, Elem)
                                                 for elem in content])))
-    
-class Html(Elem):
-    def __init__(self, content=None, attr={}):
-        super().__init__('html', attr, content, 'double')
-
-class Head(Elem):
-    def __init__(self, content=None, attr={}):
-        super().__init__('head', attr, content, 'double')
-
-class Body(Elem):
-    def __init__(self, content=None, attr={}):
-        super().__init__('body', attr, content, 'double')
-
-class Title(Elem):
-    def __init__(self, content=None, attr={}):
-        super().__init__('title', attr, content, 'double')
-
-class Meta(Elem):
-    def __init__(self, content=None, attr={}):  #no content?
-        super().__init__('meta', attr, content, 'simple')
-
-class Img(Elem):
-    def __init__(self, content=None, attr={}):  #no content?
-        super().__init__('img', attr, content, 'simple')
-
-class Table(Elem):
-    def __init__(self, content=None, attr={}):
-        super().__init__('table', attr, content, 'double')
-
-class Th(Elem):
-    def __init__(self, content=None, attr={}):
-        super().__init__('th', attr, content, 'double')
-
-class Tr(Elem):
-    def __init__(self, content=None, attr={}):
-        super().__init__('tr', attr, content, 'double')
-
-class Td(Elem):
-    def __init__(self, content=None, attr={}):
-        super().__init__('td', attr, content, 'double')
-
-class Ul(Elem):
-    def __init__(self, content=None, attr={}):
-        super().__init__('ul', attr, content, 'double')
-
-class Ol(Elem):
-    def __init__(self, content=None, attr={}):
-        super().__init__('ol', attr, content, 'double')
-
-class Li(Elem):
-    def __init__(self, content=None, attr={}):
-        super().__init__('li', attr, content, 'double')
-
-class H1(Elem):
-    def __init__(self, content=None, attr={}):
-        super().__init__('h1', attr, content, 'double')
-
-class H2(Elem):
-    def __init__(self, content=None, attr={}):
-        super().__init__('h2', attr, content, 'double')
-
-class P(Elem):
-    def __init__(self, content=None, attr={}):
-        super().__init__('p', attr, content, 'double')
-
-class Div(Elem):
-    def __init__(self, content=None, attr={}):
-        super().__init__('div', attr, content, 'double')
-
-class Span(Elem):
-    def __init__(self, content=None, attr={}):
-        super().__init__('span', attr, content, 'double')
-
-class Hr(Elem):
-    def __init__(self, content=None, attr={}):  #no content?
-        super().__init__('hr', attr, content, 'simple')
-
-class Br(Elem):
-    def __init__(self, content=None, attr={}):  #no content?
-        super().__init__('br', attr, content, 'simple')
-
 
 
 if __name__ == '__main__':
     try:
-        print(Html([Head(), Body()]))
-        
-        print('')
-
-        print(Html([Head(Title(Text('"Hello ground!"'))), 
-                    Body([H1(Text('"Oh no, not again!"')), 
-                          Img(attr={'src':'http://i.imgur.com/pfp3T.jpg',})])]))
+        print(str(Elem(tag='html', 
+                       content=[Elem(tag='head', 
+                                     content=Elem(tag='title', content=Text('"Hello ground!"'))), 
+                                Elem(tag='body', 
+                                     content=[Elem(tag='h1', content=Text('"Oh no, not again!"')),
+                                              Elem(tag='img', attr={'src':'http://i.imgur.com/pfp3T.jpg',}, tag_type='simple')])])))
     except Exception as e:
         print(e)
-    
