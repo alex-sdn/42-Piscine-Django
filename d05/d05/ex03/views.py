7aefff8fa377269c975db3d672630769bceeef8a
alex-sdn <alexsdnwcz@gmail.com>
2024-09-03 13:08:10 +0200

    update populate view+page

--- d05/d05/ex03/views.py
+++ d05/d05/ex03/views.py
@@ -5,18 +5,30 @@ from .models import Movies
 def populate(request):
     try:
         movies = [
-            Movies(episode_nb=1, title='The Phantom Menace', director='George Lucas', producer='Rick McCallum', release_date='1999-05-19'),
-            Movies(episode_nb=2, title='Attack of the Clones', director='George Lucas', producer='Rick McCallum', release_date='2002-05-16'),
-            Movies(episode_nb=3, title='Revenge of the Sith', director='George Lucas', producer='Rick McCallum', release_date='2005-05-19'),
-            Movies(episode_nb=4, title='A New Hope', director='George Lucas', producer='Gary Kurtz, Rick McCallum', release_date='1977-05-25'),
-            Movies(episode_nb=5, title='The Empire Strikes Back', director='Irvin Kershner', producer='Gary Kutz, Rick McCallum', release_date='1980-05-17'),
-            Movies(episode_nb=6, title='Return of the Jedi', director='Richard Marquand', producer='Howard G. Kazanjian, George Lucas, Rick McCallum', release_date='1983-05-25'),
-            Movies(episode_nb=7, title='The Force Awakens', director='J. J. Abrams', producer='Kathleen Kennedy, J. J. Abrams, Bryan Burk', release_date='2015-12-11'),
+            [1, 'The Phantom Menace', 'George Lucas', 'Rick McCallum', '1999-05-19'],
+            [2, 'Attack of the Clones', 'George Lucas', 'Rick McCallum', '2002-05-16'],
+            [3, 'Revenge of the Sith', 'George Lucas', 'Rick McCallum', '2005-05-19'],
+            [4, 'A New Hope', 'George Lucas', 'Gary Kurtz, Rick McCallum', '1977-05-25'],
+            [5, 'The Empire Strikes Back', 'Irvin Kershner', 'Gary Kutz, Rick McCallum', '1980-05-17'],
+            [6, 'Return of the Jedi', 'Richard Marquand', 'Howard G. Kazanjian, George Lucas, Rick McCallum', '1983-05-25'],
+            [7, 'The Force Awakens', 'J. J. Abrams', 'Kathleen Kennedy, J. J. Abrams, Bryan Burk', '2015-12-11'],
         ]
+        status = []
 
-        Movies.objects.bulk_create(movies)
+        for movie in movies:
+            try:
+                Movies.objects.create(
+                    episode_nb=movie[0],
+                    title=movie[1],
+                    director=movie[2],
+                    producer=movie[3],
+                    release_date=movie[4]
+                )
+                status.append('OK')
+            except Exception as e:
+                status.append(f'Error: {str(e)}')
 
-        return HttpResponse('OK')
+        return render(request, 'ex03/populate.html', {'status': status})
 
     except Exception as e:
         return HttpResponse(f'Error: {str(e)}')
