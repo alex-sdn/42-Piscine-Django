dd7af7679679fc67ad0f5993f4e816c49b09e698
alex-sdn <alexsdnwcz@gmail.com>
2024-10-11 18:41:09 +0200

    add fr translation

--- d07/d07/d07/settings.py
+++ d07/d07/d07/settings.py
@@ -39,6 +39,7 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    'django.middleware.locale.LocaleMiddleware',
 ]
 
 ROOT_URLCONF = 'd07.urls'
@@ -95,13 +96,21 @@ AUTH_PASSWORD_VALIDATORS = [
 # Internationalization
 # https://docs.djangoproject.com/en/4.2/topics/i18n/
 
-LANGUAGE_CODE = 'en-us'
+LANGUAGE_CODE = 'en'
 
 TIME_ZONE = 'UTC'
 
 USE_I18N = True
 
 USE_TZ = True
+
+LANGUAGES = [
+    ('en', 'English'),
+    ('fr', 'Français')
+]
+
+SITE_ROOT = os.path.dirname(os.path.realpath(__name__))
+LOCALE_PATHS = ( os.path.join(SITE_ROOT, 'locale'), )
 
 
 # Static files (CSS, JavaScript, Images)
--- d07/d07/d07/urls.py
+++ d07/d07/d07/urls.py
@@ -1,10 +1,15 @@
 from django.contrib import admin
 from django.urls import path, include
+from django.conf.urls.i18n import i18n_patterns
 from django.views.generic import RedirectView
 
 urlpatterns = [
+    path('i18n/', include('django.conf.urls.i18n')),
+]
+
+urlpatterns += i18n_patterns(
     path('admin/', admin.site.urls),
     path('articles/', include('articles.urls')),
     path('user/', include('users.urls')),
     path('', RedirectView.as_view(url='/articles/', permanent=True), name='home')
-]
+)
